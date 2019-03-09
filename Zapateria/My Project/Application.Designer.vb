5defc650d85916f50b73778b8978e301336e910c
maivi <maximilianokadyszyn@gmail.com>
2019-03-09 19:57:37 -0300

    Voy a seguir con productos porque están enlazados

--- Zapateria/My Project/Application.Designer.vb
+++ Zapateria/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Zapateria.frmLogin
+            Me.MainForm = Global.Zapateria.frmVenta
         End Sub
     End Class
 End Namespace
